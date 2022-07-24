b660189e60f70c30099262a3024f3848189995bc
Santiago Losa <slosa@bluedraft.com.ar>
2022-07-24 20:53:52 -0300

    serializer validations for Oder details and orders

--- basicecommerceproject/core_ecommerce_api/serializers.py
+++ basicecommerceproject/core_ecommerce_api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .utils import check_duplicates_in_list
 from core_ecommerce.models import Product, Order, OrderDetail
 
 
@@ -14,9 +15,18 @@ class OrderDetailSerializer(serializers.ModelSerializer):
         fields = '__all__'
         extra_kwargs = {'order': {'read_only': True}}
 
+    def validate(self, data):
+        """
+        Custom validations for each order detail
+        """
+        ok, error_message = data['product'].check_stock(data['quantity'])
+        if not ok:
+            raise serializers.ValidationError(error_message)
+        return data
+
     def create(self, validated_data):
         print("entra create order detail serializer")
-        # asd
+        asd
         return super().create(validated_data)
 
 
@@ -27,12 +37,24 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = '__all__'
 
+    def validate(self, data):
+        """
+        Custom validations for order
+        """
+        products_values = [order_detail['product'] for order_detail in data['order_details']]
+        if check_duplicates_in_list(products_values):
+            raise serializers.ValidationError('There cant be repeated products in the same order, just make one order detail with the total quantity')
+        return data
+
     def create(self, validated_data):
-        print("validated_data", validated_data)
         order_details_data = validated_data.pop('order_details')
         order = Order.objects.create(**validated_data)
         for order_detail_data in order_details_data:
-            OrderDetail.objects.create(order=order, **order_detail_data)
+            """We now know it's been validated that order details dont have repeated
+            products and also there is enough stock to create each order, we now
+            create each one and update the stock of each product"""
+            new_order_detail = OrderDetail.objects.create(order=order, **order_detail_data)
+            new_order_detail.product.update_stock(new_order_detail.quantity)
         return order
 
         # return super().create(validated_data)
