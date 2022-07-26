305230c385136974159cc562ee009b2fa3407202
Santiago Losa <slosa@bluedraft.com.ar>
2022-07-26 14:56:30 -0300

    quickfix

--- basicecommerceproject/core_ecommerce_api/helpers.py
+++ basicecommerceproject/core_ecommerce_api/helpers.py
@@ -13,19 +13,19 @@ class UDSPriceGetter:
             lines = c.readlines()
             if not lines:
                 return False
-            if check_age_of_data(lines[0].strip(), self.refresh_cache_time_in_minutes):
-                return lines[1].strip()
+            updated = check_age_of_data(lines[0].strip(), self.refresh_cache_time_in_minutes):
+            return updated, lines[1].strip()
 
     def get_price_of_dollar(self):
-        blue_value = self.get_cached_price_of_blue()
-        if not blue_value:
+        updated, blue_value = self.get_cached_price_of_blue()
+        if not updated:
             try:
                 response = requests.get(self.url).json()
                 for dolar_type in response:
                     if dolar_type['casa']['nombre'] == self.usd_type:
                         blue_value = dolar_type['casa']['venta']
                         date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                        with open('core_ecommerce_api/cache_files/price_cache.txt', 'w') as c:
+                        with open(self.cache_dir, 'w') as c:
                             c.write(f"{date}\n")
                             c.write(blue_value)
                         break
