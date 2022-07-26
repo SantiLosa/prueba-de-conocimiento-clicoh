6e7b25066e5ecfe2018171d83ff544a9c38d52fe
Santiago Losa <slosa@bluedraft.com.ar>
2022-07-26 14:44:12 -0300

    proxies and paths for python anywhere

--- basicecommerceproject/core_ecommerce_api/helpers.py
+++ basicecommerceproject/core_ecommerce_api/helpers.py
@@ -6,7 +6,11 @@ class UDSPriceGetter:
     url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
     usd_type = 'Dolar Blue'
     refresh_cache_time_in_minutes = 10
-    cache_dir = 'core_ecommerce_api/cache_files/price_cache.txt'
+    cache_dir = '/home/santilosa2295/prueba-de-conocimiento-clicoh/basicecommerceproject/core_ecommerce_api/cache_files/price_cache.txt'
+    proxies = {
+        'http': 'http://santilosa2295.pythonanywhere.com/',
+        'https': 'https://santilosa2295.pythonanywhere.com/',
+    }
 
     def get_cached_price_of_blue(self):
         with open(self.cache_dir) as c:
@@ -19,12 +23,12 @@ class UDSPriceGetter:
     def get_price_of_dollar(self):
         blue_value = self.get_cached_price_of_blue()
         if not blue_value:
-            response = requests.get(self.url).json()
+            response = requests.get(self.url, proxies=self.proxies).json()
             for dolar_type in response:
                 if dolar_type['casa']['nombre'] == self.usd_type:
                     blue_value = dolar_type['casa']['venta']
                     date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                    with open('core_ecommerce_api/cache_files/price_cache.txt', 'w') as c:
+                    with open(self.cache_dir, 'w') as c:
                         c.write(f"{date}\n")
                         c.write(blue_value)
                     break
